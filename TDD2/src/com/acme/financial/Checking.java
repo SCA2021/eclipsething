e0504c7aa2fe246b99480542e4568231dbed1ea1
Sam <saaron@una.edu>
2023-02-15 16:15:21 -0600

    Add files via upload

---
+++ TDD2/src/com/acme/financial/Checking.java
@@ -0,0 +1,8 @@
+package com.acme.financial;
+
+public class Checking extends Account {
+	public Checking (int startingBalance) {
+		super(startingBalance);
+		//auto-generated constructor stub
+	}
+}
---
+++ TDD2/src/com/acme/financial/Saving.java
@@ -0,0 +1,8 @@
+package com.acme.financial;
+
+public class Saving extends Account {
+	public Saving (int startingBalance) {
+		super(startingBalance);
+		//auto-generated constructor stub
+	}
+}
